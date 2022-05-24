9b1ce5780a99e21765644f48ce3ad220353d4d6c
xxxcrel <xxxcrel@gmail.com>
2022-05-24 22:33:56 +0800

    Add: config dubbo application and test

--- push/src/main/java/com/cheeseocean/cheeseim/push/service/PushServiceImpl.java
+++ push/src/main/java/com/cheeseocean/cheeseim/push/service/PushServiceImpl.java
@@ -1,13 +1,16 @@
 package com.cheeseocean.cheeseim.push.service;
 
+import org.apache.dubbo.config.annotation.DubboService;
+
 import com.cheeseocean.cheeseim.push.api.PushMsgReq;
 import com.cheeseocean.cheeseim.push.api.PushMsgResp;
 import com.cheeseocean.cheeseim.push.api.PushService;
 
+@DubboService
 public class PushServiceImpl implements PushService {
-
     @Override
     public PushMsgResp pushMsg(PushMsgReq pushMsgReq) {
+        System.out.println("hello");
         return PushMsgResp.newBuilder().resultCode(0).build();
     }
 }
